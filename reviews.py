3b18e74437705dc7f8d6ad4f46f7a5b8ccbc2624
Shawn chiu <shawn20111207@gmail.com>
2024-08-06 18:19:56 +0800

    add filter

--- reviews.py
+++ reviews.py
@@ -9,5 +9,12 @@ with open ('reviews.txt', 'r') as f:
 sum_len = 0
 for d in data:
 	sum_len += len(d)
-print('留言的平均長度為',sum_len / len(d),)
+print('留言的平均長度為',sum_len / len(data))
+new = []
+for d in data:
+	if len(d) < 100:
+	    new.append(d)
+print('這裡面總共有',len(new) ,'筆留言，長度小於100。')
+print(new[0])  
+print(new[1])  
 
