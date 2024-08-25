18b191f613c3633531658a0b21ce0123441558e8
Shawn chiu <shawn20111207@gmail.com>
2024-08-25 13:48:31 +0800

    add word count

--- reviews.py
+++ reviews.py
@@ -6,15 +6,29 @@ with open ('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-sum_len = 0
-for d in data:
-	sum_len += len(d)
-print('留言的平均長度為',sum_len / len(data))
-new = []
-for d in data:
-	if len(d) < 100:
-	    new.append(d)
-print('這裡面總共有',len(new) ,'筆留言，長度小於100。')
-print(new[0])  
-print(new[1])  
+print('檔案讀取完了,總共有', len(data),'筆留言')
+print(data[0])
 
+# 文字計數
+wc = {}
+for d in data:
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+for word in wc:
+	if wc[word] > 10000:
+		print(word, wc[word])
+print(len(wc))
+
+while True:
+	word = input('請問你要查什麼字: ')
+	if word == 'q':
+		break
+	elif word in wc:
+		print(wc[word])
+	elif word not in wc:
+		print('找不到這個字喔!!!')
+print('感謝使用本查詢功能!!!')
